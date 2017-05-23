b8951919b32065150993a56a7832a0a75609d36c
gonbe34 <gonbe34@gmail.com>
2017-05-23 21:00:36 +0900

    お金関係を雑に作っておく & リネーム

---
+++ src/Eard/Meu.php
@@ -0,0 +1,31 @@
+<?php
+namespace Eard;
+
+
+/****
+*
+*	通貨に関する記述
+*/
+class Meu {
+	
+	public static function getCurrencyName(){
+		return "ミュー";
+	}
+
+
+	/**
+	 * プレイヤーから政府に金渡す 
+	 */
+	public static function giveMeuFromPlayerToGoverment($fromPlayerData, $meu){
+		$fromPlayerData
+	}
+	public static function giveMeuFromGovermentToPlayer($toPlayerData, $meu){
+		
+	}
+
+	public static function giveMeuFromPlayerToPlayer($fromPlayerData, $toPlayerData, $meu){
+
+	}
+
+
+}
