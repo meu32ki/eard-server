f61bfefa1438a0959e186e3ee3bd5d1042a5627b
gonbe34 <gonbe34@gmail.com>
2017-07-03 13:28:58 +0900

    Meu: 説明の追加と、PlayerDataを格納しないように。

--- src/Eard/Meu.php
+++ src/Eard/Meu.php
@@ -8,50 +8,84 @@ namespace Eard;
 */
 class Meu {
 
-	//いまんとこplayerdataを入れておくメリットをあまり感じない 20170616
+	//いまんとこPlayerDataを入れておくメリットをあまり感じない 20170616
+	//PlayerDataをUniqueNoにした。会社からの送金があるかもしれない。20170702
 
 	/**
-	*	@param Int $amount
-	*	@param Account $playerData
+	*	@param int $amount | そのmeuの量
+	*	@param int $uniqueNo | そのmeuを持つ持ち主のUniqueNo
 	*	@return class Meu
 	*/
-	public static function get($amount, $playerData){
+	public static function get($amount, $uniqueNo){
 		$meu = new Meu();
 		$meu->amount = $amount;
-		$meu->playerData = $playerData;
+		$meu->uniqueNo = $uniqueNo;
 		return $meu;
+	}
+
+	//getにしておく必要性もあまり感じないが
+	public function getUniqueNo(){
+		return $this->uniqueNo;
 	}
 
 	public function getName(){
 		return "{$this->amount}μ";
 	}
 
+
+	/**
+	*	!注意! governmentから以外では使うな。
+	*	お金量をコントロールできるのは政府だけだから。
+	*	基本的には、meuの計算合算は外部でおこなうべき(?) 20170701
+	*	@param int $amount
+	*	@return bool
+	*/
+	public function setAmount($amount){
+		$this->amount = $amount;
+		return true;
+	}
+
+	/**
+	*	@return int
+	*/
 	public function getAmount(){
 		return $this->amount;
 	}
 
+
 	/**
-	*	分割する。playerの全額面のなかから、一部だけを切り取りたい時に。
-	*	@param Int
-	*	@return Meu
-	*/
-	public function spilit($spilitAmount){
-		$this->amount = $this->amount - $spilitAmount;
-		return new Meu($spilitAmount, $this->playerData);
-	}
-
-	public function marge($meu){
-		$this->amount = $this->amount + $meu->getAmount();
-		return true;
-	}
-
-	/*
-	*	amount以上あるか
+	*	amount以上あるか確認する
+	*	@param int $amount
+	*	@return bool
 	*/
 	public function sufficient($amount){
 		return $amount <= $this->amount;
 	}
 
-	private $amount, $playerData;
+	/**
+	*	分割する。playerの全額面のなかから、一部だけを切り取りたい時に。
+	*	@param int | 取り出したいmeu
+	*	@return Meu　or false
+	*/
+	public function spilit($spilitAmount){
+		if($this->amount < $spilitAmount){//残りが0以下にならないように
+			$this->amount = $this->amount - $spilitAmount;
+			return self::get($spilitAmount, $this->uniqueNo);
+		}else{
+			return false;
+		}
+	}
+
+	/**
+	*	合算する。
+	*	@param Meu | 吸収するMeu
+	*	@return bool
+	*/
+	public function merge(Meu $meu){
+		$this->amount = $this->amount + $meu->getAmount();
+		return true;
+	}
+
+	private $amount, $uniqueNo;
 
 }
