1504cdee8be820bc92866fcf5f96e34c6ea1a3a3
gonbe34 <gonbe34@gmail.com>
2017-08-11 02:41:46 +0900

    データディレクトリの場所移動

--- src/Eard/Utils/DataIO.php
+++ src/Eard/Utils/DataIO.php
@@ -11,7 +11,7 @@ class DataIO{
 
 
 	public static function init(){
-		self::$path = __DIR__."/../data/";
+		self::$path = __DIR__."/../../../../../EardData/";
 		if(!file_exists(self::$path)){
 			@mkdir(self::$path);
 		}
