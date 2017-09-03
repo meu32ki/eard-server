b9227fa1ba1955938aa99ad4c43ee93397b19a9a
moyasan <ryo9010602@gmail.com>
2017-09-03 14:06:11 +0900

    NPCを召喚できるように

--- src/Eard/Enemys/NPC.php
+++ src/Eard/Enemys/NPC.php
@@ -1,7 +1,51 @@
 <?php
 namespace Eard\Enemys;
 
+use pocketmine\entity\Human;
 
-class NPC extends Humanoid{
-	
+/***
+*
+*	NPCの設置だけできるやつ
+*/
+class NPC extends Human{
+
+	public static function summon($level, $x, $y, $z, $skinData, $skinId, $custom_name = null){
+		$nbt = new CompoundTag("", [
+			"Pos" => new ListTag("Pos", [
+				new DoubleTag("", $x),
+				new DoubleTag("", $y),
+				new DoubleTag("", $z)
+			]),
+			"Motion" => new ListTag("Motion", [
+				new DoubleTag("", 0),
+				new DoubleTag("", 0),
+				new DoubleTag("", 0)
+			]),
+			"Rotation" => new ListTag("Rotation", [
+				new FloatTag("", lcg_value() * 360),
+				new FloatTag("", 0)
+			]),
+			"Skin" => new CompoundTag("Skin", [
+				new StringTag("Data", $skinData),
+				new StringTag("Name", $skinId)
+			]),
+		]);
+		if(!is_null($custom_name)){
+			$nbt->CustomName = new StringTag("CustomName", $custom_name);
+		}
+		$entity = new NPC($level, $nbt);
+
+		if($entity instanceof Entity){
+			$entity->spawnToAll();
+			return $entity;
+		}
+		echo $custom_name." is not Entity\n";
+		return false;
+	}
+
+	public function __construct(Level $level, CompoundTag $nbt){
+		parent::__construct($level, $nbt);
+		$this->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_CAN_SHOW_NAMETAG, true);
+		$this->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_ALWAYS_SHOW_NAMETAG, true);
+	}
 }
