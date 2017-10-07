9348ad43f2dd4c67caa132eefb11a4e1c69b45f2
WAKAME <wakame186@gmail.com>
2017-10-08 04:33:45 +0900

    モデルするようにした

--- src/Eard/Enemys/Crosser.php
+++ src/Eard/Enemys/Crosser.php
@@ -183,8 +183,8 @@ class Crosser extends Humanoid implements Enemy{
 				new FloatTag("", 0)
 			]),
 			"Skin" => new CompoundTag("Skin", [
-				new StringTag("geometryData", EnemyRegister::loadModelData('humanoid')),
-				new StringTag("geometryName", 'geometry.humanoid'),
+				new StringTag("geometryData", EnemyRegister::loadModelData('kuwagata')),
+				new StringTag("geometryName", 'geometry.MagicTheGathering.NicolBolas'),
 				new StringTag("capeData", ''),
 				new StringTag("Data", EnemyRegister::loadSkinData('Crosser')),
 				new StringTag("Name", 'MagicTheGathering_MagicTheGatheringNicolBolas')
--- src/Eard/Enemys/Hopper.php
+++ src/Eard/Enemys/Hopper.php
@@ -186,7 +186,7 @@ class Hopper extends Humanoid implements Enemy{
 			]),
 			"Skin" => new CompoundTag("Skin", [
 				new StringTag("geometryData", EnemyRegister::loadModelData('batta')),
-				new StringTag("geometryName", '"skin.JourneyToTheWest.sha_wujing"'),
+				new StringTag("geometryName", 'skin.JourneyToTheWest.sha_wujing'),
 				new StringTag("capeData", ''),
 				new StringTag("Data", EnemyRegister::loadSkinData('Hopper')),
 				new StringTag("Name", 'JTTW_JTTWShaWujing')
