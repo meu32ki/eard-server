eafac8eb46d38a9b0ac2d244ae7423d565c198bc
moyasan <ryo9010602@gmail.com>
2017-10-14 16:21:29 +0900

    カマキリのAI変更

--- src/Eard/Enemys/Kamakiri.php
+++ src/Eard/Enemys/Kamakiri.php
@@ -21,6 +21,7 @@ use pocketmine\level\MovingObjectPosition;
 use pocketmine\level\format\FullChunk;
 use pocketmine\level\generator\normal\eardbiome\Biome;
 use pocketmine\level\particle\DestroyBlockParticle;
+use pocketmine\level\particle\SpellParticle;
 
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\DoubleTag;
@@ -57,7 +58,7 @@ class Kamakiri extends Humanoid implements Enemy{
 
 	//最大HPを取得
 	public static function getHP(){
-		return 125;
+		return 150;
 	}
 
 	//ドロップするアイテムIDの配列を取得 [[ID, data, amount, percent], [ID, data, amount, percent], ...]
@@ -122,7 +123,7 @@ class Kamakiri extends Humanoid implements Enemy{
 	}
 
 	public static function getMVPTable(){
-		return [100, 1,
+		return [100, 2,
 			[
 				[Item::SOUL_SAND, 0, 1],
 				[Item::MOSSY_COBBLESTONE, 0, 1],
@@ -165,7 +166,7 @@ class Kamakiri extends Humanoid implements Enemy{
 			Biome::MESA_PLATEAU_F => true,
 			Biome::MESA_PLATEAU => true,
 			//雨あり
-			Biome::OCEAN => true,
+			//Biome::OCEAN => true,
 			//Biome::PLAINS => true,
 			Biome::MOUNTAINS => true,
 			Biome::FOREST => true,
@@ -173,7 +174,7 @@ class Kamakiri extends Humanoid implements Enemy{
 			Biome::SWAMP => true,
 			//Biome::RIVER => true,
 			//Biome::ICE_PLAINS => true,
-			Biome::SMALL_MOUNTAINS => true,
+			//Biome::SMALL_MOUNTAINS => true,
 			Biome::BIRCH_FOREST => true,
 		];
 	}
@@ -228,7 +229,8 @@ class Kamakiri extends Humanoid implements Enemy{
 		$this->target = false;
 		$this->charge = 0;
 		$this->mode = 0;
-		$this->pitch = 90;
+		$this->walk = true;
+		$this->walkSpeed = 0.15;
 		//$this->setSneaking(1);
 		/*$item = Item::get(267);
 		$this->getInventory()->setItemInHand($item);*/
@@ -237,39 +239,129 @@ class Kamakiri extends Humanoid implements Enemy{
 	public function onUpdate(int $tick): bool{
 		if($this->getHealth() > 0 && AI::getRate($this)){
 			if($this->target = AI::searchTarget($this)){
-				AI::lookAt($this, $this->target);
-				//$this->pitch += 90;
-				AI::rangeAttack($this, 3.0, 3, null, function ($a, $v){
-					$ev = new EntityDamageByEntityEvent($a, $v, EntityDamageByEntityEvent::CAUSE_ENTITY_ATTACK, 1, 0);
-					$task = new DelayAttack($a, $v, $ev);
-					Server::getInstance()->getScheduler()->scheduleDelayedTask($task, 12);
-					Server::getInstance()->getScheduler()->scheduleDelayedTask($task, 15);
-					Server::getInstance()->getScheduler()->scheduleDelayedTask($task, 18);
-					$v->heal(new EntityRegainHealthEvent($v, 0, EntityRegainHealthEvent::CAUSE_MAGIC));
-					return true;
-				});
-				AI::setRate($this, 15);
+				//AI::lookAt($this, $this->target);
+				switch($this->mode){
+					case 0:
+						$this->mode = mt_rand(1, 2);
+						$this->charge = 0;
+						$this->walk = true;
+						AI::setRate($this, 10);
+					break;
+					case 1://バックステップから突進攻撃
+						switch($this->charge){
+							case 0:
+								AI::lookAt($this, $this->target);
+								AI::jump($this, -0.55, 0, 0.5);
+								$this->walk = false;
+							case 1:
+							case 2:
+							case 3:
+								++$this->charge;
+								$this->level->addParticle(new SpellParticle($this, 220, 20, 20));
+								AI::setRate($this, 5);
+							break;
+							case 4:
+								$this->walk = true;
+								$this->walkSpeed = 0.35;
+								AI::lookAt($this, $this->target);
+							case 5:
+							case 6:
+							case 7:
+							case 8:
+							case 9:
+							case 10:
+							case 11:
+							case 12:
+							case 13:
+							case 14:
+							case 15:
+								AI::setRate($this, 3);
+								$this->level->addParticle(new SpellParticle($this, 20, 120, 20));
+								$this->rangeAttack(3.0, 3, ceil(4 - ($this->getHealth()/$this->getMaxHealth()/0.25)));
+								++$this->charge;
+							break;
+							case 16:
+								AI::lookAt($this, $this->target);
+								AI::setRate($this, 20);
+								$this->walkSpeed = 0.15;
+								$this->mode = 0;
+							break;
+						}
+					break;
+					case 2://乱舞
+						switch($this->charge){
+							case 0:
+								AI::lookAt($this, $this->target);
+								$this->walk = false;
+							case 1:
+							case 2:
+							case 3:
+								++$this->charge;
+								$this->level->addParticle(new SpellParticle($this, 220, 120, 20));
+								AI::setRate($this, 5);
+							break;
+							case 4:
+								$this->walk = false;
+								AI::lookAt($this, $this->target);
+							case 5:
+							case 6:
+							case 7:
+							case 8:
+							case 9:
+								AI::jump($this, 1.25, 0, 0.1);
+								AI::setRate($this, 10, 4.5);
+								$this->level->addParticle(new SpellParticle($this, 220, 180, 20));
+								$this->rangeAttack($this->charge, 4, 4);
+								++$this->charge;
+								$this->yaw += mt_rand(0, 360);
+							break;
+							case 10:
+								AI::lookAt($this, $this->target);
+								AI::setRate($this, 20);
+								$this->walkSpeed = 0.15;
+								$this->mode = 0;
+							break;
+						}
+					break;
+					default :
+						$this->mode = 0;
+					break;
+				}
 			}else{
 				$this->yaw += mt_rand(-60, 60);
 				AI::setRate($this, 40);
 			}
 		}
-		$can = AI::walkFront($this, 0.15);
-		if(!$can){
-			AI::jump($this);
+		if($this->walk){
+			$can = AI::walkFront($this, $this->walkSpeed);
+			if(!$can){
+				AI::jump($this);
+			}
 		}
 		return parent::onUpdate($tick);
+	}
+
+	public function rangeAttack($damage = 3.0, $range = 3, $more = 3){
+		AI::rangeAttack($this, $range, $damage, null, function ($a, $v) use ($more){
+			$ev = new EntityDamageByEntityEvent($a, $v, EntityDamageByEntityEvent::CAUSE_ENTITY_ATTACK, 1, 0);
+			$task = new DelayAttack($a, $v, $ev);
+			for($i = 0; $i < $more; $i++){
+				Server::getInstance()->getScheduler()->scheduleDelayedTask($task, 20+$i*2);
+			}
+			$v->heal(new EntityRegainHealthEvent($v, 0, EntityRegainHealthEvent::CAUSE_MAGIC));
+			return true;
+		});
 	}
 
 	public function attackTo(EntityDamageEvent $source){
 		$victim = $source->getEntity();
 		$source->setKnockBack(0.1);
 		if(!$victim->isSneaking()) $source->setKnockBack(-0.1);
-		$pk = new AnimatePacket();
+/*		$pk = new AnimatePacket();
 		////$pk->eid = $this->getId();
 		$pk->entityRuntimeId = $this->getId();
 		$pk->action = 1;//ArmSwing
-		Server::getInstance()->broadcastPacket($this->getViewers(), $pk);
+		Server::getInstance()->broadcastPacket($this->getViewers(), $pk);*/
 		$victim->level->addparticle(new DestroyBlockParticle($victim, Block::get(152)));
 	}
 
